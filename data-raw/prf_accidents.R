cdb5901184c6cfab18ad4b3d3747d4d4edf8297b
pabsantos <pabsantos21@gmail.com>
2023-09-04 12:13:28 -0300

    Load and save

--- data-raw/prf_accidents.R
+++ data-raw/prf_accidents.R
@@ -1,15 +1,25 @@
 library(data.table)
+library(lubridate)
 
 file_list <- paste0(
   "unzip -p data-raw/",
   list.files("data-raw", pattern = "^datatran")
 )
 
-sinistros_list <- lapply(file_list, fread, encoding = "Latin-1")
+accidents_list <- lapply(file_list, fread, encoding = "Latin-1")
 
-sinistros_list[[1]][, data_inversa := lubridate::dmy(data_inversa)]
+fix_date <- function(accidents) {
+  if (!is.Date(accidents$data_inversa)) {
+    accidents[, data_inversa := dmy(data_inversa)]
+  } else {
+    accidents[, data_inversa := ymd(data_inversa)]
+  }
+}
 
+lapply(accidents_list, fix_date)
 
-rbindlist(sinistros_list, fill = TRUE)
+prf_accidents <- rbindlist(accidents_list, fill = TRUE)
 
-usethis::use_data(sinistros, overwrite = TRUE)
+prf_accidents[, id := as.character(id)]
+
+usethis::use_data(prf_accidents, overwrite = TRUE)
